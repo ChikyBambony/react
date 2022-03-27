7d76bf81bb6afdc5b3ad19c5b31c72cfbb425dc6
chikybambony <assault108@gmailcom>
2022-03-27 13:26:29 +0300

    first todo

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+const el = {
+    <div>    
+    
+    
+    </div>    
+} 
+console.log(el);
+
+ReactDOM.render(el, document.getElementById('root'));
